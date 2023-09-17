194b5292707ac77505d955dd6992a0c227d409f3
Zhihare <128489441+Zhihare@users.noreply.github.com>
2023-09-17 14:28:52 +0300

    17.09

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,25 @@ import data from './components/Statistics/data';
 import friends from './components/FriendList/friends';
 import transactions from './components/TransactionHistory/transactions';
 
+import css from './App.module.css'
 
 export const App = () => {
   return (
-    <div>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <Friendlist friends={friends} />
-      <TransactionHistory items={transactions} />
+    <div className={css.container}>
+      <div className={css.leftSection}>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+        <Friendlist friends={friends} />
+      </div>
+      <div className={css.rightSection}>
+        <Statistics title="Upload stats" stats={data} />
+        <TransactionHistory items={transactions} />
+      </div>
     </div>
 
 
